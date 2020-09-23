8544394f6fef002dfffce0385fc20a256b76467c
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-09-23 18:45:04 +0300

    rename Script. Update HTML4 CSS tables

--- JS/script2.js
+++
@@ -1,4 +0,0 @@
-// 2020.09.21 Look with HTML4 
-var el = document.getElementById(`heading1`);
-el.style.color = "green";
-console.log(el)
---
+++ JS/script4.js
@@ -0,0 +1,5 @@
+// 2020.09.21 Look with HTML4 
+// Styling with JS, not CSS
+var el = document.getElementById(`heading3`);
+el.style.color = "blue";
+console.log(el)
