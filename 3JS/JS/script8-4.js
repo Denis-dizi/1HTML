64907cbbc034983871626e5cd477d73c1f29d43e
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-09-30 22:10:54 +0300

    update JS lectory 8-4

--- 3JS/JS/script8-4.js
+++ 3JS/JS/script8-4.js
@@ -1,4 +1,11 @@
+// W3D1 2020.09.28
+// (2:04:)
+
 console.group(`Array, Object`);
+// (2:09:)
+// Array
+console.log('Array:');
+
 var arr = [
     1,          // index = 0
     2,          // index = 1
@@ -7,41 +14,48 @@ var arr = [
 ]
 
 var obj = {
-
 }
 
 console.log(arr);
-console.log(`arr[2]: `+ arr[2]);
+console.log(`Get element from array by index: arr[2]:`);
 console.log(arr[2]);
 
 console.log(obj);
+console.log('= = = = = = = = = = = =');
 
-// (2:14:)
+// (2:13:)
+// Object
+console.log('Object car:');
 var car = {
-    color: `white`,
+    color: `white`,        // ipashiba
     wheels: 4,
-    makeBeep: function(){
+    makeBeep: function(){ //funkcija ieks objekta - metode
         console.log(`Beep`)
     }
 }
 
-console.log('car:');
+// (2:21:)
 console.log(car);
 console.log('car color: ' + car.color);
 console.log('car wheels: ' + car['wheels']);
-
 var parametr = 'wheels'
 console.log('car wheels: ' + car[parametr]);
 
+console.log('= = = = = = = = = = = =');
 console.log('Date:');
-var d = new Date();
-
+//(2:17:)
+var d = new Date(); // Veido objektu uz klases pamata
+                    //Klase "Date", izsaucam  "konstruktor" funkciju, funkcija ievieto "d" mainigajaa objektu.
+console.log(d);
+console.log(d.getDay());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
 console.groupEnd();
 console.log('= = = = = = = = = = = =');
 
 
-console.group(`Function`);
-
+console.group(`Person`);
+// (2:22:)
 var person = {
     name: 'Jane',
     age: 24,
@@ -51,12 +65,17 @@ var person = {
         return this.age
     },
 }
+
+console.log(person);
 console.log(person.greet());
 console.groupEnd();
 console.log('= = = = = = = = = = = =');
 
+// (2:31:)
 function clickMe(){
-    console.log(clickMe)
+    // console.log(this)
+    document.getElementById("demo").innerHTML = "Hello World";
+    // document.getElementById("demo1").innerHTML = "YOU CLICKED ME!"
 }
 
 console.log('= = = = = = = = = = = =');
