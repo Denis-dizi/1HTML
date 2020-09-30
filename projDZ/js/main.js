c26a71aa5a3bed29da9cda67c61fe517d456c3d1
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-09-30 23:14:40 +0300

    updated weather DZ proj lectory

--- projDZ/js/main.js
+++ projDZ/js/main.js
@@ -1,15 +1,34 @@
+// W3D2  2020.09.29
+
+// (1:43:)
+let cities = {Riga: 'Rīga'}
+
+// (1:18:)
 var abj = {}
 var weather = document.getElementById('weather');
 
 
 fetch('http://127.0.0.1/api/inbox/')
+// C:\xampp\htdocs\api\inbox
   .then(res => res.json())
   .then(data => callback(data))
   .catch(e => console.log(e))
 
+  // (1:21:)
   function callback(data){
-    obj = data
-    var out = `<h4 class="center">${data.geoip.city}</h4><h5 class="center">${data.weather.currTemp}&#8451;</h5>`
-    weather.innerHTML = out
+    console.log(data);
+    obj = data;
+    var out = 
+    // `<h4 class="center">${data.geoip.city}</h4>
+    // <p class="center">
+    // `<h4 class="center">${data.geoip.city}</h4>
+    `<h4 class="center">${cities[data.geoip.city] || data.geoip.city}</h4>
+    <p>
+    Temperatura: ${data.weather.currTemp}&#8451;<br>
+    Veja virziens: ${data.weather.windDir};<br>
+    Veja atrums: ${data.weather.windSpeed};<br>
+    Debesis: ${data.weather.icoSymbol};<br>
+    </p>`;
+    weather.innerHTML = out;
   
   }
