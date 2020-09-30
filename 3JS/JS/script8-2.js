f9ac2def5d60c8d5835fbe772f4c76246ed22a52
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-09-30 20:46:20 +0300

    lecture update

--- 3JS/JS/script8-2.js
+++ 3JS/JS/script8-2.js
@@ -1,12 +1,15 @@
-// primitivie
+// W3D1 2020.09.28
+// (0:54:)
+// Data Types. Primitive, Complex.
+
+// primitivie - dod vertibas
 // string, number, boolean, undefined
 
-
-// complex
+// complex - dod atsuces uz obj
 // function, object
 
+// (1:08:)
 var a = "hello";
-
 var b = {
     a: `hello`
 }
@@ -23,3 +26,26 @@ function hello2(obj) {
 }
 console.log(a);
 console.log(b);
+console.log('= = = = = = = = = = = = = = = = =');
+
+
+// (1:12:)
+// https://www.youtube.com/watch?time_continue=779&v=x2RNw4M6cME&feature=emb_logos
+
+// (1:12:)
+var c = 5;
+var d = { c: 10 }
+
+hello3(c);
+hello4(d);
+
+function hello3(par) {
+    par = 20
+}
+
+function hello4(obj) {
+    obj.c = 30
+}
+console.log(c); //5
+console.log(d); //obj c:30
+console.log('= = = = = = = = = = = = = = = = =');
