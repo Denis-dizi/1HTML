ccd22a9bafffe11c8d735a5cc575daf026eaf2d0
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-01 20:18:48 +0300

    updated lucture

--- 3JS/W3D3/js/stl2.js
+++ 3JS/W3D3/js/stl2.js
@@ -7,12 +7,15 @@ fetch('https://jsonplaceholder.typicode.com/posts')
 
   function renderTable(obj){
       data = obj
-      let out = ''
+      let out = ``
+      let j = 0
   
   var arrLength = obj.length
-  for (let i = 0; i < arrLength; i++){
-      out += `<h3>${obj[i].title}</h3>`
-      out += `<p>${obj[i].body}</p>`
+
+    while(j < arrLength){
+      out += `<h3>${obj[j].title}</h3>
+      <p>${obj[j].body}</p>`
+      j++ // j = j + 1, j += 1
+    }
+    posts.innerHTML = out
   }
-  posts.innerHTML = out
-}
