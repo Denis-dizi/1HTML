c991f6c32204eb36e35fca63951f2784b2a70d1a
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-02 21:06:06 +0300

    before new files for table

--- 3JS/W3D4_JS/js/script2.js
+++ 3JS/W3D4_JS/js/script2.js
@@ -1,26 +1,56 @@
 //W3D4   2020.10.02
 //jQuery
-const modal = document.getElementById('modal')
+
+// jQuery code
+
+// jQuery code
+
+// jQuery code
 
 $(document).ready(function(){
-  // console.log($('#but1'))
-  $('#but1').click(function(){
-    console.log('I\'m clicked by jQuery')
-    $('#text1').toggle(700)
+    // console.log($('#but1'))
+    $('#text2').hide()
+  
+    $('#but1').click(function(){
+      console.log('I\'m clicked by jQuery')
+      $('#text1').slideToggle(700)
+      $('#text2').slideToggle(700)
+    })
+  
+    $.get('https://jsonplaceholder.typicode.com/users', function(data, status){
+      console.log(data)
+      console.log(status)
+    })
+  
+    $('#fsubmit').click(()=>{
+      $.post("http://192.168.1.151/test.php",
+      {
+        name: $('#user').val(),
+        city: $('#pass').val()
+      },
+      function(data, status){
+        console.log("Data: " + data + "\nStatus: " + status);
+        if(status == 'success') {
+          location.reload()
+        }
+      })
+    })
   })
-})
-
-// onclick event from native js
-document.getElementById('but2').onclick = function(){
-  console.log('Onclick event')
-  modal.classList.toggle('showed')
-}
-
-// addEventListener from native js
-document.getElementById('but3').addEventListener('click', function(){
-  console.log('Onclick event by addEventListener method')
-})
-
-function clickFunction(){
-  console.log('Button\'s onclick event')
-}
+  
+  // NATIVE JS
+  const modal = document.getElementById('modal')
+  
+  // onclick event from native js
+  document.getElementById('but2').onclick = function(){
+    console.log('Onclick event')
+    modal.classList.toggle('showed')
+  }
+  
+  // addEventListener from native js
+  document.getElementById('but3').addEventListener('click', function(){
+    console.log('Onclick event by addEventListener method')
+  })
+  
+  function clickFunction(){
+    console.log('Button\'s onclick event')
+  }
