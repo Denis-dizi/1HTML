da28c7dd22e3ce93addb2fd69b510a161ed6f710
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-02 18:49:48 +0300

    new files for W3D4

---
+++ 3JS/W3D4_JS/js/script1.js
@@ -0,0 +1,14 @@
+//W3D4   2020.10.02
+//
+
+const obj = {
+    name: `Ann`,
+    surname: `Hendricks`,
+    age: 23,
+    location: `London`
+}
+
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
