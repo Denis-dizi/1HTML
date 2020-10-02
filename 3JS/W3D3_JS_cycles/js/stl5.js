ae70a83a713cd72e616aff29a435e7ce4ee832c1
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-02 12:52:36 +0300

    For of cycle lecture update

--- 3JS/W3D3_JS_cycles/js/stl5.js
+++ 3JS/W3D3_JS_cycles/js/stl5.js
@@ -1,14 +1,23 @@
+// (0:47:) video 2
+// For of cycle
+
 function renderTable(obj){
   data = obj
   let out = ''
   let j = 0
-  // var arrLength = obj.length
 
   // obj.forEach(element => {
   //   out += `<h3>${element.title}</h3>
   //     <p>${element.body}</p>
   //     <p>post ID: ${element.id}</p>`
   // });
+
+  // for(let index in obj) {
+  //   out += `<h3>${obj[index].title}</h3>
+  //     <p>${obj[index].body}</p>
+  //     <p>ForIn</p>
+  //     <p>post ID: ${obj[index].id}</p>`
+  // }
 
   for(let element of obj) {
     out += `<h3>${element.title}</h3>
