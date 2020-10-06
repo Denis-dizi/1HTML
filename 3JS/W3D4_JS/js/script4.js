b5df535856682ba2fc3baa629eed02cdfd1709ea
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-07 00:52:14 +0300

    arr join - update lecture

--- 3JS/W3D4_JS/js/script4.js
+++ 3JS/W3D4_JS/js/script4.js
@@ -1,2 +1,16 @@
-//W3D4   2020.10.02
-//
+//W3D4-4   2020.10.02
+//(2:45:)
+
+
+const arr1 = ['Hello', "world"]
+
+console.group('Array join method')
+console.log('arr1:')
+console.log(arr1)
+console.log('arr1.join(\' \'):')
+console.log('\t')
+console.log(arr1.join(''))
+console.log(arr1.join(' '))
+console.log(arr1.join('-'))
+console.log(arr1.join('_'))
+console.groupEnd()
