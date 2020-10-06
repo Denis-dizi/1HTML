0272328c791b426cff0e016141b5fa85afe24164
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-06 15:52:40 +0300

    lectory updateL Object.keys, values, entries

--- 3JS/W3D4_JS/js/script1.js
+++ 3JS/W3D4_JS/js/script1.js
@@ -1,6 +1,6 @@
 //W3D4   2020.10.02
 //
-
+console.log('Object.keys, values, entries');
 const obj = {
     name: `Ann`,
     surname: `Hendricks`,
@@ -8,9 +8,16 @@ const obj = {
     location: `London`
 }
 
-console.log(Object.keys(obj))
-console.log(Object.values(obj))
-console.log(Object.entries(obj))
+console.log('obj:');
+console.log(obj);
+console.log('Object.keys(obj):');
+console.log(Object.keys(obj));
+console.log('Object.values(obj):');
+console.log(Object.values(obj));
+console.log('Object.entries(obj):');
+console.log(Object.entries(obj));
+console.log('= = = = = = = = = = = = = = = = ');
+
 
 // $.get({
 
