35c4feeefe2633f7982a4b764b662ea0b232cae6
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-10 00:58:51 +0300

    W3D3 update lecture

--- 3JS/W3D3_JS_cycles/js/script10-1.js
+++ 3JS/W3D3_JS_cycles/js/script10-1.js
@@ -1,4 +1,5 @@
 // W3D3 2020.10.01
+// (0:16:) video 1
 
 // AJAX = Asynchronous JavaScript And XML.
 // AJAX is not a programming language.
@@ -6,6 +7,33 @@
 // A browser built-in XMLHttpRequest object (to request data from a web server)
 // JavaScript and HTML DOM (to display or use the data)
 
-// CSV - Comma-separated values
+// AJAX
+// https://www.w3schools.com/xml/ajax_intro.asp
+// AJAX = Asynchronous JavaScript And XML.
+// AJAX is not a programming language.
+// AJAX is a technique for accessing web servers from a web page.
+
+// XML
+// https://www.w3schools.com/xml/default.asp
+// XML stands for eXtensible Markup Language.
+// XML was designed to store and transport data.
+// XML was designed to be both human- and machine-readable.
+// tieši datu glabašanai. Kalpo tiem pašiem nolukiem kā JSON
+// free naming tags. If tag is broken XML will continue to work, JSON will stop.
+
+
+// CSV
+// comma-separated values) file is a text file that has a 
+// specific format which allows data to be saved in a table structured format.
 // Name; Surname; Salary
 // Thomas; Newman; 1000
+
+
+// JSON		
+// W3D2 (0:01:)  
+// https://www.json.org/json-en.html
+// https://www.w3schools.com/js/js_json_intro.asp
+// JSON is a syntax for storing and exchanging data.
+// JSON is text, written with JavaScript object notation.
+// If you have data stored in a JavaScript object, you can convert the object into JSON, and send it to a server:
+// If you receive data in JSON format, you can convert it into a JavaScript object:
