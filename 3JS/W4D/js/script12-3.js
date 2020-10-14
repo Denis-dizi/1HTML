a4f1d9c983300a8122d78d101f4b058f7e198953
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-14 23:45:32 +0300

    lectures W4. Moove, rename files, folders.

---
+++ 3JS/W4D/js/script12-3.js
@@ -0,0 +1,14 @@
+// W4D1
+
+const arr = [];
+console.log(`arr =`);
+console.log(arr);
+
+console.log(`arr1 =`);
+const arr1 = [1,2,3];
+console.log(arr1);
+
+console.log(`arr1[1] =`);
+console.log(arr1[1]);
+console.log(`arr1.length =`);
+console.log(arr1.length);
---
+++ 3JS/W4D/js/script13-1.js
@@ -0,0 +1,9 @@
+// W4D2
+// data tupes 2
+
+function sayHello () {
+    alert(`Alloha!`);
+}
+console.log(sayHello);
+alert(sayHello);
+
