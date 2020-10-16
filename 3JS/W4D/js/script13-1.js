3664aa7f8e97ccd42e218b7d90acb3155282283d
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-16 15:32:59 +0300

    update lectory W4D2

--- 3JS/W4D/js/script13-1.js
+++ 3JS/W4D/js/script13-1.js
@@ -2,8 +2,18 @@
 // data tupes 2
 
 function sayHello () {
-    alert(`Alloha!`);
+    return(`Alloha!`);
 }
-console.log(sayHello);
-alert(sayHello);
+console.log(sayHello());
+// alert(sayHello);
 
+
+
+//W03L01 - JavaScript data types, variables, conditions.
+//2020.02.11
+//(1:14:)
+
+var callName = function(name){
+    return("My name is " +name)
+}
+console.log(callName("DiZi"));
