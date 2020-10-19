7796644915fbb7eb6b255f77b346cf64aa1beaf3
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-19 19:19:06 +0300

    update reapet of W4D3 lecture

--- 3JS/W4D/js/script15-1.js
+++ 3JS/W4D/js/script15-1.js
@@ -1,3 +1,41 @@
 // WD5D1
 // 2020.10.19
 
+//Repeat W4D3
+
+let obj = {test: 1, test1: 2};
+Object.values(obj);
+
+
+// let.obj1 = Object.create({});
+// obj1;
+let arr = new Array();
+arr;
+
+function MyArray(...params) {
+    const arr = []
+    params.forEach((val)=>{
+        arr.push(val)
+    })
+    return arr;
+}
+
+//Create your own prototype
+// function Food (name, price, amount) {
+// }
+
+function Food(nem, price, amount) {
+    this.name = name;
+    this.price = price;
+    this.amount = amount;
+}
+
+
+let pizza = new Food ("pizza", 7, 3);
+// pizza.getTotalSum();
+
+let burger = new Food ("burger", 5, 30);
+
+//DOM
+//(0:47:)
+
