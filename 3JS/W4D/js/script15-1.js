0d3b6a91056caf1c9ea08119cae9f93e1999d8c5
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-20 00:57:16 +0300

    W5D1 lectory update

--- 3JS/W4D/js/script15-1.js
+++ 3JS/W4D/js/script15-1.js
@@ -39,3 +39,73 @@ let burger = new Food ("burger", 5, 30);
 //DOM
 //(0:47:)
 
+//localStorage
+// (0:54:)
+
+//Window
+// (0:59)
+let win = window.open("", "test", {targe: "blank"})
+// win.resizeTo(500, 500);
+
+
+let win2 = window.open("", "", "");
+// win2.document;
+
+// win2.document.write('<h1 id="title">I\'m a title</h1>')
+
+// win2.document.getElementById
+
+// [0].addEventListener('hover', function(e) {
+//     console.log(e);
+// })
+
+// let win3 = window.open();
+// win3.document.write("<button>Click me to close</button>")
+
+// win3.document.getElementsByTagName("button")[0].addEventListener("click", function(){
+//     win3.close();
+// })
+
+
+// = = =  Task = = = 
+const userNames = [];
+userNames.push("Ed");
+userNames.push("Jon");
+userNames.push("Jim");
+userNames.push("bill");
+
+let win4 = window.open();
+
+userNames.forEach(function(value, index) {
+    win4.document.write("<h1>Element in array with" + index + " is " +value +"</h1>");
+})
+
+win4.document;
+win4.document.getElementsByTagName("h1");
+Object.values(win4.document.getElementsByTagName("h1"));
+
+win4.document.getElementsByTagName("h1").forEach();
+
+win4.document.getElementsByTagName("h1")[0].classList;
+win4.document.getElementsByTagName("h1")[0].classList.add("title");
+win4.document.getElementsByTagName("h1")[0].classList.remoove("title");
+win4.document.getElementsByTagName("h1")[0].classList.toggle("title");
+win4.document.getElementsByTagName("h1")[0].classList.toggle("cool");
+win4.document.getElementsByTagName("h1")[0].removeAttribute("class");
+win4.document.getElementsByTagName("h1")[0].setAttribute("what-a-cool-atribut", "test");
+win4.document.getElementById("first");
+win4.document.getElementById("first").classList.add("one", "two");
+
+
+//cookies vs localStorage
+//(2:09:)
+document.cookie = "nosaukums = vertiba";
+document.cookie;
+localStorage;
+localStorage.clear();
+localStorage.saraksts = {test: 1, test1: 2};
+localStorage.saraksts;
+localStorage.saraksts = JSON.stringify({test: 1, test1: 2});
+localStorage.saraksts;
+JSON.parse(localStorage.saraksts);
+localStorage.users = [];
