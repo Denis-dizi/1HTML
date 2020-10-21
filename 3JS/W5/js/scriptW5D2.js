27924c0ae6e49830ad5e128e29c23d26ab889084
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-21 19:59:01 +0300

    update

--- 3JS/W5/js/scriptW5D2.js
+++ 3JS/W5/js/scriptW5D2.js
@@ -15,9 +15,28 @@ console.log('loader')
 function isFormValid(form) {
     let isFormValid = true;
 
+    const errorMsgBlock =  document.getElementsByClassName('error-msg');
+    Object.values(errorMsgBlock).forEach(function(block) {
+        block.innerHTML = ''
+    })
+
+
     const username = form.namedItem('username').value;
-    if (username.length < 6)
+    if (username.length < 6){
+        const errorMsg =  document.getElementsByClassName('error-msg username')[0];
+        errorMsg.innerHTML = "Min 6 characters for username"
         isFormValid = false;
+    }
+
+    const email = form.namedItem('email').value;
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!re.test()) {
+        const errorMsg =  document.getElementsByClassName('error-msg email')[0];
+        errorMsg.innerHTML = "Not a valid email"
+        isFormValid = false;
+
+    }
 
     console.log(username);
     return isFormValid;
