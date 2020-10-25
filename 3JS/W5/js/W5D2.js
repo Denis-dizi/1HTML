ca3ff5b6e9e58d5e928b24ab6964640efea93409
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-10-25 21:56:43 +0200

    aded: Delete button, other corrections

--- 3JS/W5/js/W5D2.js
+++ 3JS/W5/js/W5D2.js
@@ -68,21 +68,24 @@ window.addEventListener('load', function () { //because of this comand script li
 
     function renderTable() { //(1:40:)
 
-        const table = document.getElementById("user-table");
+        const table = document.getElementById("users-table");
+        //(2:55:)
+        const tBody = table.getElementsByTagName('tbody')[0];
+        tBody.innerHTML = '';
         // const userList = JSON.parse(localStorage.userList); //(2:31:) changed
         const userList = localStorage.userList ? JSON.parse(localStorage.userList) : [];
 
         userList.forEach(function (user, index) { //(1:48:)
             user = JSON.parse(user) // from string to object
-            table.innerHTML += `
+            tBody.innerHTML += `
         <tr>
             <td>` + user.username + `</td>
             <td>` + user.email + `</td>
             <td> 
                 <button class = "edit-btn" user-id = ` + index + ` >Edit</button>
                 <button class = "delete-btn" user-id = ` + index + `>Delete</button>
-                </td>
-                </tr>`;
+            </td>
+        </tr>`;
             //(2:28:)
             // <button class = "delete-btn" title = ` + index + `>Delete</button>
         })
@@ -106,6 +109,26 @@ window.addEventListener('load', function () { //because of this comand script li
                 form.namedItem('user-id').value = userId
 
                 console.log(user);
+            })
+        })
+        // (0:08: W5D3)
+        const deleteBtns = document.getElementsByClassName('delete-btn');
+
+        Object.values(deleteBtns).forEach(function (btn) {
+            btn.addEventListener('click', function (e_click) {
+                const userId = e_click.target.getAttribute('user-id')
+                console.log('Delete btn presed')
+                const userList = JSON.parse(localStorage.userList);
+                //(0:15: W5D3)
+                userList.splice(userId, 1); //erase one element by index 'userId'
+                //save changed array
+                localStorage.userList = JSON.stringify(userList);
+
+                const table = document.getElementById('users-table');
+                const tBody = table.getElementsByTagName('tbody')[0];
+                const tRowToDelete = tBody.getElementsByTagName('tr')[userId];
+
+                tRowToDelete.innerHTML = '';
             })
         })
     }
