82175e3488f479f0c0630477be7e2ff7248ff932
Deniss Zhuravskis <davidovzzz@gmail.com>
2020-11-29 23:00:48 +0200

    update (1:00:) db connected. Works.

--- 6Node/nodejs/app.js
+++ 6Node/nodejs/app.js
@@ -1,7 +1,34 @@
 // (:23:)W10D2
+const {
+    response
+} = require('express');
 const express = require('express');
+const mysql = require('mysql');
 
 const app = new express();
+const router = express.Router();
+
+router.get('', (reject, response) => {
+    console.log("Response was sent to the browser.")
+    response.send("Requested route does not exist.");
+});
+
+app.use('*', router);
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: '',
+    database: 'shop-i' //xampp must be started
+})
+
+db.connect((error) => {
+    if (error) {
+        throw error
+    }
+    console.log("Connected to database");
+})
 
 app.listen(8002, () => {
     console.log('Fine!! Server is up on localhost on http://localhost:8002') //not a browsers console
